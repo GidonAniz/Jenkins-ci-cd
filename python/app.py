85488724f8f57c98a80f6c06081ba64680619d27
Gidon Aniz <113261408+GidonAniz@users.noreply.github.com>
2023-11-12 23:57:52 +0200

    Update app.py

--- python/app.py
+++ python/app.py
@@ -1,30 +1,42 @@
 import boto3
 
-ec2 = boto3.client('ec2')
+def get_instance_information():
+    # Set AWS credentials using the environment variables
+    session = boto3.Session(
+        aws_access_key_id='YOUR_ACCESS_KEY',
+        aws_secret_access_key='YOUR_SECRET_KEY',
+        region_name='YOUR_REGION'
+    )
+    
+    ec2 = session.client('ec2')
 
-# Use the client to get information about instances
-response = ec2.describe_instances(
-    Filters=[
-        {
-            'Name': 'instance-state-code',
-            'Values': ['16']
-        },
-        {
-            'Name': 'tag:k8s.io/role/master',
-            'Values': ['1']
-        }
-    ]
-)
+    # Use the client to get information about instances
+    response = ec2.describe_instances(
+        Filters=[
+            {
+                'Name': 'instance-state-code',
+                'Values': ['16']
+            },
+            {
+                'Name': 'tag:k8s.io/role/master',
+                'Values': ['1']
+            }
+        ]
+    )
 
-# Extract the instance information from the response
-instances = []
-for reservation in response['Reservations']:
-    for instance in reservation['Instances']:
-        instances.append(instance)
+    # Extract the instance information from the response
+    instances = []
+    for reservation in response['Reservations']:
+        for instance in reservation['Instances']:
+            instances.append(instance)
 
-# Print the instance IDs and instance names
-for instance in instances:
-    instance_id = instance['InstanceId']
-    instance_name = next((tag['Value']
-                          for tag in instance['Tags'] if tag['Key'] == 'Name'), 'Unnamed')
-    print("Instance ID: {}, Instance Name: {}".format(instance_id, instance_name))
+    # Return the instance IDs and instance names
+    return [(instance['InstanceId'], next((tag['Value'] for tag in instance['Tags'] if tag['Key'] == 'Name'), 'Unnamed')) for instance in instances]
+
+if __name__ == "__main__":
+    instance_info = get_instance_information()
+
+    # Print the instance IDs and instance names
+    for instance_id, instance_name in instance_info:
+        print(f"Instance ID: {instance_id}, Instance Name: {instance_name}")
+
