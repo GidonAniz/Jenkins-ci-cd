ce30a928cc785bb66b9ad8a0e948889b228e59b9
Gidon Aniz <113261408+GidonAniz@users.noreply.github.com>
2023-11-18 20:17:23 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,42 +1,47 @@
-
 import boto3
+import schedule
+import time
+import os
 
 def get_instance_information():
-    # Set AWS credentials using the environment variables
-    session = boto3.Session(
-        aws_access_key_id='AKIA5XC7JSBWTTL763BI',
-        aws_secret_access_key='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',
-        region_name='us-east-1'
-    )
-    
-    ec2 = session.client('ec2')
-
-    # Use the client to get information about instances
-    response = ec2.describe_instances(
-        Filters=[
-            {
-                'Name': 'instance-state-code',
-                'Values': ['16']
-            },
-            {
-                'Name': 'tag:k8s.io/role/master',
-                'Values': ['1']
-            }
-        ]
-    )
-
-    # Extract the instance information from the response
-    instances = []
-    for reservation in response['Reservations']:
-        for instance in reservation['Instances']:
-            instances.append(instance)
-
-    # Return the instance IDs and instance names
-    return [(instance['InstanceId'], next((tag['Value'] for tag in instance['Tags'] if tag['Key'] == 'Name'), 'Unnamed')) for instance in instances]
+    # Your existing code here...
 
 if __name__ == "__main__":
-    instance_info = get_instance_information()
+    # Set up the interval from the environment variable, default to 5 minutes
+    interval_minutes = int(os.getenv("INTERVAL_MINUTES", 5))
 
-    # Print the instance IDs and instance names
-    for instance_id, instance_name in instance_info:
-        print(f"Instance ID: {instance_id}, Instance Name: {instance_name}")
+    # Schedule the function to run every specified interval
+    schedule.every(interval_minutes).minutes.do(get_instance_information)
+
+    while True:
+        # Run scheduled jobs
+        schedule.run_pending()
+        time.sleep(1)
+
+        # Check if there is a response with IPs
+        if instance_info:
+            # Count the number of clusters
+            num_clusters = len(instance_info)
+
+            # Log the information in JSON format (modify as needed)
+            log_data = {
+                "threadName": "MainThread",
+                "name": "K8S REPORTS",
+                "time": time.strftime("%m/%d/%Y"),
+                "Running clusters": num_clusters
+            }
+
+            # Log individual cluster information
+            for idx, (instance_id, instance_name) in enumerate(instance_info, start=1):
+                log_data[f"CLUSTER {idx} IP"] = instance_id
+                log_data[f"CLUSTER {idx} NAME"] = instance_name
+
+            log_data["msecs"] = time.time() * 1000
+            log_data["message"] = "testing K8S REPORTING"
+            log_data["levelname"] = "INFO"
+
+            # Output logs as JSON (modify as needed)
+            print(json.dumps(log_data))
+
+        # Reset instance_info after logging
+        instance_info = []
