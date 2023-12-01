48141614c3474e90e31c9d6e4225db21beea276a
Gidon Aniz <113261408+GidonAniz@users.noreply.github.com>
2023-12-01 15:42:24 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,18 @@
-
 import boto3
+import os
+import schedule
+import time
+import json
+
 
 def get_instance_information():
-    # Set AWS credentials using the environment variables
+    # Set AWS credentials using the hardcoded values
     session = boto3.Session(
         aws_access_key_id='AKIA5XC7JSBWTTL763BI',
         aws_secret_access_key='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',
         region_name='us-east-1'
     )
-    
+
     ec2 = session.client('ec2')
 
     # Use the client to get information about instances
@@ -31,12 +35,28 @@ def get_instance_information():
         for instance in reservation['Instances']:
             instances.append(instance)
 
-    # Return the instance IDs and instance names
-    return [(instance['InstanceId'], next((tag['Value'] for tag in instance['Tags'] if tag['Key'] == 'Name'), 'Unnamed')) for instance in instances]
+    # Return the instance information as a list of dictionaries
+    return [
+        {'InstanceId': instance['InstanceId'],
+         'InstanceName': next((tag['Value'] for tag in instance['Tags'] if tag['Key'] == 'Name'), 'Unnamed')}
+        for instance in instances]
 
-if __name__ == "__main__":
+
+def print_instance_info():
     instance_info = get_instance_information()
 
-    # Print the instance IDs and instance names
-    for instance_id, instance_name in instance_info:
-        print(f"Instance ID: {instance_id}, Instance Name: {instance_name}")
+    # Print the instance information as JSON
+    print(json.dumps(instance_info, indent=2))
+
+
+if __name__ == "__main__":
+    # Get the interval from the environment variable, defaulting to 5 minutes
+    interval_minutes = int(os.environ.get('INTERVAL_MINUTES', 0.1))
+
+    # Schedule the job to run every specified interval
+    schedule.every(interval_minutes).minutes.do(print_instance_info)
+
+    # Run the scheduler indefinitely
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
