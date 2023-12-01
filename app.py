241b2b36dd2289118ffd8fdfa264d4426ee12048
Gidon Aniz <113261408+GidonAniz@users.noreply.github.com>
2023-12-01 19:54:27 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,17 @@
 import os
 import time
 import boto3
+import logging
+
+# Configure logging
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s [%(levelname)s] - %(message)s",
+    handlers=[
+        logging.StreamHandler(),  # Print logs to the console
+        logging.FileHandler("output.log")  # Save logs to a file
+    ]
+)
 
 def get_instance_information():
     # Set AWS credentials with hardcoded values
@@ -33,7 +44,10 @@ def get_instance_information():
             instances.append(instance)
 
     # Return the instance IDs and instance names
-    return [(instance['InstanceId'], next((tag['Value'] for tag in instance['Tags'] if tag['Key'] == 'Name'), 'Unnamed')) for instance in instances]
+    return [
+        (instance['InstanceId'], next((tag['Value'] for tag in instance['Tags'] if tag['Key'] == 'Name'), 'Unnamed'))
+        for instance in instances
+    ]
 
 if __name__ == "__main__":
     interval_minutes = int(os.environ.get('INTERVAL_MINUTES', 5))
@@ -41,9 +55,9 @@ if __name__ == "__main__":
     while True:
         instance_info = get_instance_information()
 
-        # Print the instance IDs and instance names
+        # Log the instance IDs and instance names
         for instance_id, instance_name in instance_info:
-            print(f"Instance ID: {instance_id}, Instance Name: {instance_name}")
+            logging.info(f"Instance ID: {instance_id}, Instance Name: {instance_name}")
 
         # Sleep for the specified interval in minutes
         time.sleep(interval_minutes * 60)
